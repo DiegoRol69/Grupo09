ef21c56bf90ce0f074dd6a9f07126b00849dc2b2
Diego Rol Sánchez <drol@ucm.es>
2021-04-19 12:21:08 +0200

    Añadidos módulos para customizar el color

---
+++ DIEGOYMIRIAM/Colores/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Colores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2+2);
+            Console.BackgroundColor = ConsoleColor.Green;
+        }
+    }
+}
