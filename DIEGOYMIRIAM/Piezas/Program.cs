f87691ee3baea04b1ea1cc60cf13862456917eda
LAPTOP-BDKK6IT9\miria <miriammarinsanchez2002@gmail.com>
2021-04-20 17:15:00 +0200

    Creada la funcionalidad para rotar piezas

---
+++ DIEGOYMIRIAM/Piezas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Piezas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("se mueven las piezas");
+        }
+    }
+}
